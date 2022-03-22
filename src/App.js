46ef5e07e216259c9ca6364714d08ae630f98939
Victor Kodzo Seayrom <mrseayrom@gmail.com>
2022-03-22 14:41:40 +0000

    react component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import writers from "./writers";
-
+import { ProfileCard } from "./ProfileCard";
 function App() {
   return (
     <div>
       <h1>WRITER PROFILES</h1>
       <div className="container">
         {writers.map((writer) => (
-          <div className="card">
-            <img
-              src={`/images/${writer.avatar}.png`}
-              width="300px"
-              height="300px"
-              alt=""
-            />
-            <div className="textGroup">
-              <h3>{writer.name}</h3>
-              <p>{writer.email}</p>
-              <p>{writer.phone}</p>
-            </div>
-          </div>
+          <ProfileCard writer={writer} />
         ))}
       </div>
     </div>
