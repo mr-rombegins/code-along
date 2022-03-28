d2d0863e68223e1898f9c5de794654def83de369
Victor Kodzo Seayrom <mrseayrom@gmail.com>
2022-03-28 17:01:41 +0000

    react-component-5

--- public/src/App.js
+++ public/src/App.js
@@ -3,32 +3,35 @@ import "./App.css";
 import ProfileCard from "./component/ProfileCard";
 import ProfileForm from "./component/ProfileForm";
 
-function App() {
-  const [allProfile, setAllProfile] = useState([
-    {
-      firstName: "star",
-      lastName: "Emak",
-      email: "star@gmail.com",
-      phone: "+233 2456789",
-    },
-  ]);
 
-  const submit = (profile) => {
-    const arr = allProfile;
-    arr.push(profile);
-    setAllProfile(arr);
-  };
+function App() {
+
+  const [allProfile, setAllProfile] = useState([{
+    firstName:"star",
+    lastName:"Emak",
+    email:"star@gmail.com",
+    phone:"+233 2456789"
+  }])
+
+  const submit =(profile)=>{
+    const arr = allProfile
+    arr.push(profile)
+    setAllProfile(arr)
+  }
 
   return (
     <div className="App">
       <h1>Writer Profiles.</h1>
       <div className="container">
-        <ProfileForm submit={submit} />
-        {allProfile.map((writer) => (
-          <ProfileCard key={writer.id} writer={writer} />
-        ))}
+        <ProfileForm submit={submit}/>
+        {
+          allProfile.map((writer)=>(
+            <ProfileCard key={writer.id} writer={writer}/>
+          ))
+        }
       </div>
     </div>
+
   );
 }
 
