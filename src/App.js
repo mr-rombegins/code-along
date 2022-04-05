8e6ac019e7c5c403d8a88886beb4ed27fb353ac0
Victor Kodzo Seayrom <mrseayrom@gmail.com>
2022-04-05 14:16:37 +0000

    react-component-9

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./component/navbar/Navbar";
 import About from "./pages/About";
 import Blogs from "./pages/Blog";
+import BlogList from "./pages/BlogList";
+import BlogDetail from "./pages/BlogDetail";
 
 import Home from "./pages/Home";
 
@@ -11,9 +13,12 @@ const App = () => {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="about" element={<About />} />
-        <Route path="blog" element={<Blogs />} />
         <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="blog" element={<Blogs />}>
+          <Route path="" element={<BlogList />} />
+          <Route path=":blog" element={<BlogDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,38 +1,7 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
 
-const Blogs = () => {
-  return (
-    <section>
-      <h1>Welcome to the Blog Page</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, in
-        minus quas repellat similique quis ratione qui totam alias dolorum aut?
-        Provident laborum recusandae ipsam! Distinctio incidunt nulla error
-        voluptatum! Debitis, odio aliquid repudiandae laboriosam dolor eligendi
-        voluptatum corporis temporibus tenetur culpa ullam accusamus quos
-        exercitationem cumque, voluptate cum. Eligendi dolore quisquam omnis
-        sequi culpa cum facilis esse distinctio eos? Nesciunt velit
-        necessitatibus blanditiis fugiat saepe numquam quae in perferendis
-        deleniti veniam, alias quaerat asperiores natus officia optio animi
-        voluptas totam mollitia quasi iste, nihil sed tempora dolore excepturi!
-        Quod!
-      </p>
+function Blog() {
+  return <Outlet />;
+}
 
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, in
-        minus quas repellat similique quis ratione qui totam alias dolorum aut?
-        Provident laborum recusandae ipsam! Distinctio incidunt nulla error
-        voluptatum! Debitis, odio aliquid repudiandae laboriosam dolor eligendi
-        voluptatum corporis temporibus tenetur culpa ullam accusamus quos
-        exercitationem cumque, voluptate cum. Eligendi dolore quisquam omnis
-        sequi culpa cum facilis esse distinctio eos? Nesciunt velit
-        necessitatibus blanditiis fugiat saepe numquam quae in perferendis
-        deleniti veniam, alias quaerat asperiores natus officia optio animi
-        voluptas totam mollitia quasi iste, nihil sed tempora dolore excepturi!
-        Quod!
-      </p>
-    </section>
-  );
-};
-
-export default Blogs;
+export default Blog;
